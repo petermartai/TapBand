cd01b689be6d6c05316506c1a8dee3bc1f18a32d
balazsdan <balazs.dan@gmail.com>
2015-12-23 06:53:14 +0100

    Basic TapController and TapView classes created.

---
+++ TapBand_SRC/Assets/Script/Controllers/TapController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class TapController
+{
+    private TapView view;
+
+    public delegate void TapEvent(BigInteger value);
+    public event TapEvent OnTap;
+
+    TapController(TapView view)
+    {
+        this.view = view;
+
+        view.OnTap += HandleTap;
+    }
+
+    private void HandleTap(TapArgs args)
+    {
+        if (OnTap != null)
+        {
+            BigInteger tapValue = CalculateTapValue(args.positions.Count);
+            OnTap(tapValue);
+        }
+    }
+
+    private BigInteger CalculateTapValue(int tapCount)
+    {
+        return tapCount * 1;
+    }
+}
---
+++ TapBand_SRC/Assets/Script/Views/TapView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class TapArgs
+{
+    public ICollection<Vector3> positions;
+}
+
+public class TapView : MonoBehaviour
+{
+    private Collider2D collider;
+
+    public delegate void TapEvent(TapArgs args);
+    public event TapEvent OnTap;
+
+	// Use this for initialization
+	void Start()
+    {
+        collider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            TapArgs args = CalculateTapEventArgs();
+            if (OnTap != null)
+                OnTap(args);
+        }
+	}
+
+    private TapArgs CalculateTapEventArgs()
+    {
+        TapArgs args = new TapArgs();
+        Collider2D collider = GetComponent<Collider2D>();
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+            Touch touch = Input.GetTouch(i);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 wp = Camera.main.ScreenToWorldPoint(touch.position);
+                if (collider == Physics2D.OverlapPoint(wp))
+                {
+                    args.positions.Add(touch.position);
+                }
+            }
+        }
+
+        return args;
+    }
+}
