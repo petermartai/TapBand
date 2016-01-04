10119727ca89eac038c4f654c01c31dd3040a3a9
minimalgeek <minimalgeek@gmail.com>
2016-01-04 15:23:39 +0100

    RestartUI changes
    remove panel ref

--- TapBand_SRC/Assets/Script/Views/RestartUI.cs
+++ TapBand_SRC/Assets/Script/Views/RestartUI.cs
@@ -10,7 +10,7 @@ public class RestartUI : MonoBehaviour {
     public delegate bool RestartEnabledEvent();
     public event RestartEnabledEvent RestartEnabled;
 
-    public GameObject restartPanelButton,restartPanel,restartButton,upgradePanel;
+    public GameObject restartPanelButton,restartPanel,restartButton;
 	
 	public void Start()
 	{
@@ -21,7 +21,6 @@ public class RestartUI : MonoBehaviour {
 	{
 		restartPanel.SetActive (true);
 		restartPanelButton.SetActive (false);
-		upgradePanel.SetActive (false);
 		Time.timeScale=0;
 	}
 	
@@ -29,7 +28,6 @@ public class RestartUI : MonoBehaviour {
 	{ 
 		restartPanel.SetActive (false);
 		restartPanelButton.SetActive (true);
-		upgradePanel.SetActive (true);
         restartButton.GetComponent<Button>().interactable = false;
 		Time.timeScale=1;
 	}
